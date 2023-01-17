47d0c537e5962848b617b9c404a7bc401eaf2414
Smartino <Iniobongeffiong40@gmail.com>
2023-01-17 05:27:29 -0800

    trying to fix the error

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
+
+function printCharacters(3) {
+  const url = `https://swapi.dev/api/films/${3}/`;
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      const data = JSON.parse(body);
+      const characters = data.characters;
+      characters.forEach(function (character) {
+        request(character, function (error, response, body) {
+          if (error) {
+            console.log('Error:', error);
+          } else {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+          }
+        });
       });
-    });
-  }
-});
+    }
+  });
+}
+
